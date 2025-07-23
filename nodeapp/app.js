dbbca1ef9f73120e09f49c78a602e743acd21262
Basem Maher <basem.119maher@gmail.com>
2025-07-23 05:57:21 +0300

    first commit

---
+++ nodeapp/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("service is up and running ");
+});
+app.listen(8080,()=>{
+    console.log("Srever is up");
+});
