7e0d4c7d5714fbdffd5d6bf581e73e25cacf82dd
Basem Maher <38473368+basem119@users.noreply.github.com>
2025-07-26 15:54:36 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
   region = var.region
 }
 resource "aws_instance" "server" {
-  ami = "ami-052efd3df9dad4" #need to insure (ubuntu image) it's right from aws
+  ami = ami-0becc523130ac9d5d" #Ubuntu Server 22.04 LTS (HVM), SSD Volume Type
   instance_type = "t2.micro"
   key_name = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.maingroup.id]
