403a8f9de10d5447be755646cfd4fcc3f90f3298
Basem Maher <38473368+basem119@users.noreply.github.com>
2025-07-26 17:27:26 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 resource "aws_instance" "server" {
   ami = "ami-0becc523130ac9d5d" #Ubuntu Server 22.04 LTS (HVM), SSD Volume Type
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   key_name = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.maingroup.id]
   iam_instance_profile = aws_iam_instance_profile.ec2-profile.name
